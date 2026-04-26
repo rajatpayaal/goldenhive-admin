8a010ceedcf4c3b64ae2f21a0f20e9491062a258
rajatpayaal <rajatpayaal@users.noreply.github.com>
2026-04-27 05:06:44 +0530

    change ulr ec2

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://13.206.107.203/api'
+const BASE_URL = import.meta.env.VITE_API_URL || '/api'
 
 const api = axios.create({
   baseURL: BASE_URL,
